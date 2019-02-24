857a7d753422f40850901d1f0e02d79ba11b0675
infvie <sreenath.raparti@gmail.com>
2019-02-24 11:53:01 -0500

    added files to scr

--- src/cassette.jl
+++ src/cassette.jl
@@ -2,8 +2,10 @@ module Dubstep
 
 using Cassette
 using LinearAlgebra
+using LightGraphs
+using MetaGraphs
 export construct, TracedRun, trace, TraceCtx, LPCtx, replacenorm,
-    GraftCtx, replacefunc
+    GraftCtx, replacefunc, TypeCtx, typegraphfrompath
 
 function construct(T::Type, args...)
     @info "constructing a model $T"
@@ -140,6 +142,143 @@ see also: `bin/graft.jl` for an example.
 function replacefunc(f::Function, d::AbstractDict)
     ctx = GraftCtx(metadata=d)
     return Cassette.recurse(ctx, f)
+end
+            
+#------------------------------------------------------------------------------------           
+            
+Cassette.@context TypeCtx
+            
+"""   TypeCtx
+
+creates a MetaDiGraph tracking the types of args and ret values throughout a script
+
+"""
+TypeCtx
+            
+            
+
+"""   FCollector(depth::Int,frame::function,data::FCollector)
+
+struct to collect all the functions called throughout a script
+        
+"""
+mutable struct FCollector{I,F,C}
+    depth::I
+    frame::F
+    data::Vector{C}
+end
+            
+
+
+"""   FCollector(depth::Int,frame::Frame)
+
+this is an initialization funtion for the FCollector
+
+"""
+function FCollector(d::Int, f)
+    FCollector(d, f, FCollector[])
+end
+
+""" Frame(func, args, ret, subtrace)
+
+a structure to hold metadata for recursive type information for every function called
+            
+"""
+mutable struct Frame{F,T,U}
+    func::F
+    args::T
+    ret::U
+end
+            
+# add boilerplate for functionality
+function Cassette.overdub(ctx::TypeCtx, f, args...)
+    c = FCollector(ctx.metadata.depth-1, Frame(f, args, Any))
+    push!(ctx.metadata.data, c)
+    if c.depth > 0 && Cassette.canrecurse(ctx, f, args...)
+        newctx = Cassette.similarcontext(ctx, metadata = c)
+        z = Cassette.recurse(newctx, f, args...)
+        c.frame.ret = typeof(z)
+        return z
+    else
+        z = Cassette.fallback(ctx, f, args...)
+        c.frame.ret = typeof(z)
+        return z
+    end
+end
+
+# limit the stacktrace in terms of which to recurse on
+Cassette.canrecurse(ctx::TypeCtx,::typeof(Base.vect), args...) = false
+Cassette.canrecurse(ctx::TypeCtx,::typeof(FCollector)) = false
+Cassette.canrecurse(ctx::TypeCtx,::typeof(Frame)) = false
+
+# this is a function that strips all using statements from a raw text file
+# reasons to do so
+# 1) we no longer include libraries we dont have too so semanticmodels can remain small
+# 2) prevents errors in the include-cassette design which doesn allow for usings
+function extractdeps(filename::String)
+    f = open(filename)
+    newf = open("new$filename","w") 
+    for line in readlines(f)
+        if occursin("using",line)
+            ex = Meta.parse(line)
+            eval(ex)
+        else
+            write(newf,"$line\n")
+        end
+    end
+    close(f)
+    close(newf)
+    return "new$filename"
+end
+                
+""" buildgraph
+
+internal function used in the typegraphfrompath
+takes the collector object and returns a metagraph
+            
+"""
+function buildgraph(g,collector)
+    add_vertex!(g,:name,collector.frame.args)
+    add_vertex!(g,:name,collector.frame.ret)
+    add_edge!(g,nv(g)-1,nv(g),:name,collector.frame.func)
+    for frame in collector.data
+        buildgraph(g,frame)
+    end
+    return g
+end
+
+""" typegraph(path::AbstractString,maxdepth::Int)
+            
+This is a function that takes in an array of script and produces a MetaDiGraph descibing the system.
+takes in optional parameter of recursion depth on the stacktrace defaulted to 3
+
+"""
+function typegraph(path::AbstractString,maxdepth::Int=3)
+    
+    # init the collector object
+    extractor = FCollector(maxdepth, Frame(nothing, (), nothing,))
+                
+    # init the context we want
+    ctx = TypeCtx(metadata = extractor);
+    
+    # build new script we want and load deps in env
+    newpath = extractdeps(path)
+                    
+    # cassette requires a callable stack
+    transcribe() = include(newpath)
+                        
+    # run the script internally and build the extractor data structure
+    Cassette.overdub(ctx,transcribe);
+                        
+    # delete our file we created for eval
+    rm(newpath)
+    
+    # crete a graph where we will init our tree
+    g = MetaDiGraph()
+    
+    # pass the collector ds to make the acutal metagraph
+    return buildgraph(g,extractor)
+    
 end
 
 end #module
